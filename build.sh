716640fc7edf696f654c5f7c111c701c0f09d4f0
mAbdullah-17 <abdullahornatedhm@gmail.com>
2026-06-06 05:43:47 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,1 @@
-#!/usr/bin/env bash
-
-# Compile the standard C++ core console loop application 
-# Using -O3 optimization flag for efficient execution
-g++ -O3 library_system.cpp -o library_system
-
-# Grant execution permissions to the generated C++ binary file
-chmod +x library_system
+g++ -O3 library_system.cpp -o library_system && chmod +x library_system
